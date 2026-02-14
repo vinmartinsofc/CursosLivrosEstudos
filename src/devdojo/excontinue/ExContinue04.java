2188914ddf8edcc9e0baf79f9d50f73c4e50bd11
Vin Martins <marcosviniciossl3@gmail.com>
2026-02-14 14:48:05 -0300

    update: back to windows 11

--- src/devdojo/excontinue/ExContinue04.java
+++ src/devdojo/excontinue/ExContinue04.java
@@ -1,5 +1,25 @@
 package devdojo.excontinue;
 
 public class ExContinue04 {
+    public static void main(String[] args) {
+        int rand;
+        int invalidCount = 0;
+        int validCount = 0;
 
+        for (int i = 0; i < 100; i++) {
+            rand = (int) (Math.random() * 50001) - 2500;
+
+            if (rand < 0) {
+                invalidCount++;
+                continue;
+            }
+
+            validCount++;
+        }
+
+        System.out.println("Total valid nums: " + validCount);
+        System.out.println("Total invalid nums: " + invalidCount);
+
+
+    }
 }
