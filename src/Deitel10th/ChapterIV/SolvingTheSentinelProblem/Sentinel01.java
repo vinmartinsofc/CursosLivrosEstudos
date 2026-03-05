8f89f482851ca59b178fed197cfd94caad4fcd09
Vin Martins <marcosviniciossl3@gmail.com>
2026-03-04 22:45:30 -0300

    update: refining programming logic

--- src/Deitel10th/ChapterIV/SolvingTheSentinelProblem/Sentinel01.java
+++ src/Deitel10th/ChapterIV/SolvingTheSentinelProblem/Sentinel01.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Sentinel01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int sum = 0;
 
         System.out.print("Enter the integer or press 0 to exit: ");
@@ -13,19 +12,19 @@ public class Sentinel01 {
 
         while (userInteger != 0) {
 
-            sum += userInteger;
+            if (userInteger < 0) {
+                System.out.println("You must provide a valid number");
+            } else {
+                sum += userInteger;
+            }
 
             System.out.print("Enter the integer or press 0 to exit: ");
-
             userInteger = scanner.nextInt();
-
         }
 
-        if (sum > 0) {
-            System.out.printf("Total amount: %d%n", sum);
-        } else {
-            System.out.println("Any number had been provided");
-        }
+        System.out.printf("Total amount: %d%n", sum);
+
+
 
         scanner.close();
 
