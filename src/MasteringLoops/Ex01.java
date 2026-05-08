983f12fcdfde661fb052732cdffd429ae557666f
Vin Martins <marcosviniciossl3@gmail.com>
2026-05-08 15:36:42 -0300

    update: daily commit

--- src/MasteringLoops/Ex01.java
+++ src/MasteringLoops/Ex01.java
@@ -1,16 +1,15 @@
-package UnderstandingFor;
+package MasteringLoops;
 
 public class Ex01 {
     public static void main(String[] args) {
 
-
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("*");
+        for (int i = 0; i <= 5; i++) {
 
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
+            System.out.println();
 
         }
 
